cd0332236d9f50b1ca3a097e95b4a6545336c963
HugoAntoinee <hugo.antoine@exxact-robotics.com>
2024-05-15 14:06:47 +0200

    update cors issue

--- tourism_companion/app/main.py
+++ tourism_companion/app/main.py
@@ -1,9 +1,12 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database import engine
 from app.models import Base
 from app.routers import chat
-from dotenv import load_dotenv
-import os
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,6 +16,18 @@ print("Loaded API Key:", os.getenv("OPENAI_API_KEY"))
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Your React app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows requests from specified origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (POST, GET, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 Base.metadata.create_all(bind=engine)
 
 app.include_router(chat.router, prefix="/api/v1")
